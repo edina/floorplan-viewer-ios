f01825d75f64dff0c9c034f188cc330048bfee13
murrayk <murrayhking@gmail.com>
2015-07-20 12:51:27 +0100

    Issue #4 intialise ibeacons manager

--- FloorPlan/JCTiledViewDemo/JCAppDelegate.m
+++ FloorPlan/JCTiledViewDemo/JCAppDelegate.m
@@ -8,16 +8,29 @@
 
 #import "JCAppDelegate.h"
 #import "RootViewController.h"
+#import <EstimoteSDK/EstimoteSDK.h>
+
+@interface JCAppDelegate () <ESTBeaconManagerDelegate>
+// 3. Add a property to hold the beacon manager
+@property (nonatomic) ESTBeaconManager *beaconManager;
+@end
 
 @implementation JCAppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-  self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
-  self.window.rootViewController = [[RootViewController alloc] initWithNibName:nil bundle:nil];
-  [self.window makeKeyAndVisible];
-
-  return YES;
+    
+    self.beaconManager = [ESTBeaconManager new];
+    self.beaconManager.delegate = self;
+   
+    [self.beaconManager requestAlwaysAuthorization];
+    
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    self.window.rootViewController = [[RootViewController alloc] initWithNibName:nil bundle:nil];
+    [self.window makeKeyAndVisible];
+    
+    
+    return YES;
 }
 
 - (void)applicationWillResignActive:(UIApplication *)application
